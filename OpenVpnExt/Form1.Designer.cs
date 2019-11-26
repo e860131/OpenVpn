e916d5b6dee6f9bebdf1bb53870652da6fe6ac80
xj <xj@192.168.114.212>
2019-11-26 16:12:23 +0800

    添加项目文件。

---
+++ OpenVpnExt/Form1.Designer.cs
@@ -0,0 +1,285 @@
+﻿namespace OpenVpnExt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.toolStrip1.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripButton1,
+            this.toolStripSeparator1,
+            this.toolStripButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(758, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripButton1
+            // 
+            this.toolStripButton1.Image = global::OpenVpnExt.Properties.Resources.agree;
+            this.toolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton1.Name = "toolStripButton1";
+            this.toolStripButton1.Size = new System.Drawing.Size(52, 22);
+            this.toolStripButton1.Text = "执行";
+            this.toolStripButton1.Click += new System.EventHandler(this.toolStripButton1_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripButton2
+            // 
+            this.toolStripButton2.Image = global::OpenVpnExt.Properties.Resources.取消;
+            this.toolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripButton2.Name = "toolStripButton2";
+            this.toolStripButton2.Size = new System.Drawing.Size(52, 22);
+            this.toolStripButton2.Text = "退出";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 25);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(758, 53);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "加载配置文件";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(568, 21);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "浏览";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 23);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(461, 21);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "配置文件位置:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.textBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox2.Location = new System.Drawing.Point(0, 78);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(758, 50);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "输出文件位置";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(568, 19);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "浏览";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(101, 21);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(461, 21);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 24);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 12);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "输出文件位置:";
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 128);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.groupBox3);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.groupBox4);
+            this.splitContainer1.Size = new System.Drawing.Size(758, 320);
+            this.splitContainer1.SplitterDistance = 392;
+            this.splitContainer1.TabIndex = 5;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.richTextBox1);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 0);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(392, 320);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "执行日志";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox1.Location = new System.Drawing.Point(3, 17);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(386, 300);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.richTextBox2);
+            this.groupBox4.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox4.Location = new System.Drawing.Point(0, 0);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(362, 320);
+            this.groupBox4.TabIndex = 0;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "输出日志";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.richTextBox2.Location = new System.Drawing.Point(3, 17);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(356, 300);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(758, 448);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "主界面";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox4.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton toolStripButton1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton toolStripButton2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ OpenVpnExt/Form1.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace OpenVpnExt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// 加载配置文件按钮
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button1_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox1.Text = folder.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// 输出文件位置
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button2_Click(object sender, EventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
+            if (folder.ShowDialog() == DialogResult.OK)
+            {
+                this.textBox2.Text = folder.SelectedPath;
+            }
+        }
+        /// <summary>
+        /// 执行
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Thread Thd_Send = new Thread(new ThreadStart(exec));//新建一个线程以发送数据
+            Thd_Send.Start();
+        }
+        /// <summary>
+        /// 退出
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void exec()
+        {
+            try
+            {
+                richappendtext1(textBox1.Text.Trim());
+                richappendtext2(textBox2.Text.Trim());
+                DirectoryInfo root = new DirectoryInfo(textBox1.Text.Trim());
+                FileInfo[] files = root.GetFiles();
+                if (files.Length > 0)
+                {
+                    foreach (FileInfo fileInfo in files)
+                    {
+                        if (fileInfo.Extension.Contains(".ovpn") && (fileInfo.Name.Contains("tcp") || fileInfo.Name.Contains("udp")))
+                        {
+                            richappendtext1("执行文件名:" + fileInfo.Name);
+
+                            string str = fileInfo.Name.Split('_')[0];
+                            richappendtext1("获取的ip地址:" + str);
+                            string adrrer = GetstringIpAddress(str);
+                            richappendtext1("解析的位置:"+adrrer);
+                            string ipname = "";
+                            if (!string.IsNullOrEmpty(adrrer))
+                            {
+                                ipname = adrrer.Split(' ')[0].Substring(1, adrrer.Split(' ')[0].Length - 1);
+                            }
+
+                            richappendtext1("ip地址所在位置:" + ipname);
+
+                            var ping = new System.Net.NetworkInformation.Ping();
+
+                            var result = ping.Send(str);
+
+                            if (result.Status != System.Net.NetworkInformation.IPStatus.Success)
+                            {
+                                richappendtext1("执行失败:");
+                                richappendtext1("---------返回的状态:" + result.Status);
+                                richappendtext1("---------答复的主机地址:" + result.Address);
+                                richappendtext1("---------往返时间:" + result.RoundtripTime);
+                            }
+                            else
+                            {
+                                richappendtext1("执行成功:");
+                                richappendtext1("---------返回的状态:" + result.Status);
+                                richappendtext1("---------答复的主机地址:" + result.Address);
+                                richappendtext1("---------往返时间:" + result.RoundtripTime);
+                                richappendtext1("---------生存时间:" + result.Options.Ttl);
+                                richappendtext1("---------缓存区大小:" + result.Buffer.Length);
+
+                                richappendtext2("文件名:" + fileInfo.Name + " 执行成功");
+    
+                                richappendtext2("ip地址所在位置:" + str+" "+ ipname);
+                                //移动文件
+                                CopyDirectory(fileInfo.FullName, textBox2.Text.Trim()+ "\\"+ fileInfo.Name, "(" + ipname + ")"+fileInfo.Name);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                richappendtext1(ex.Message);
+            }
+        }
+        /// <summary>
+        /// 复制文件
+        /// </summary>
+        /// <param name="sourceDirPath"></param>
+        /// <param name="SaveDirPath"></param>
+        private  void CopyDirectory(string file, string SaveDirPath,string newfilename)
+        {
+            try
+            {
+                //如果指定的存储路径不存在，则创建该存储路径
+                if (!Directory.Exists(SaveDirPath))
+                {
+                    //创建
+                    Directory.CreateDirectory(SaveDirPath);
+                }
+                //string pFilePath = SaveDirPath + "\\" + Path.GetFileName(file);
+                //if (!File.Exists(pFilePath))
+                //{
+                //    File.Copy(file, pFilePath, true);
+                //}
+
+                string pFilePath = SaveDirPath + "\\" + newfilename;
+
+                FileInfo fi = new FileInfo(file); //xx/xx/aa.rar
+                fi.MoveTo(pFilePath); //xx/xx/xx.rar
+            }
+            catch (Exception ex)
+            {
+                richappendtext1("复制文件失败:"+ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// 获取ip对应的中文地址
+        /// </summary>
+        /// <param name="ip"></param>
+        /// <returns></returns>
+        private string GetstringIpAddress(string ip)
+        {
+            string url = "http://ipaddr.cz88.net/data.php?ip=" + ip;
+            //得到网页源码
+            string html = GetHtml(url);
+            richappendtext1("获取ip页面源码:"+html);
+            if (html.Contains("ShowIPAddr("))
+            {
+                html = html.Substring(11, html.Length - 11);
+                html = html.Substring(0, html.Length-2);
+                html= html.Split(',')[1];
+            }
+
+            return html;
+        }
+        /// <summary>
+        /// 获取HTML源码信息(Porschev)
+        /// </summary>
+        /// <param name="url">获取地址</param>
+        /// <returns>HTML源码</returns>
+        public string GetHtml(string url)
+        {
+            string str = "";
+            try
+            {
+                Uri uri = new Uri(url);
+                WebRequest wr = WebRequest.Create(uri);
+                Stream s = wr.GetResponse().GetResponseStream();
+                StreamReader sr = new StreamReader(s, Encoding.Default);
+                str = sr.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                richappendtext1("获取ip位置页面源码错误:"+e.Message);
+            }
+            return str;
+        }
+
+        private void richappendtext1(string str)
+        {
+            this.Invoke(new Action(() =>
+            {
+
+                //这边开始就是委托的内容
+                richTextBox1.AppendText(str + "\r\n");
+                richTextBox1.ScrollToCaret();
+            }));
+        }
+        private void richappendtext2(string str)
+        {
+            this.Invoke(new Action(() => {
+
+                //这边开始就是委托的内容
+                richTextBox2.AppendText(str + "\r\n");
+                richTextBox2.ScrollToCaret();
+            }));
+        }
+    }
+}
